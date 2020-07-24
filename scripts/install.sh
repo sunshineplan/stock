9e5505ae34c26854cbd4b1c918104f180ec0677a
sunshineplan <sunshineplan@users.noreply.github.com>
2020-07-24 20:01:12 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -17,7 +17,7 @@ configMyStocks() {
     read -p 'Please enter VerifyHeader header: ' header
     read -p 'Please enter VerifyHeader value: ' value
     read -p 'Please enter unix socket(default: /run/mystocks-go.sock): ' unix
-    [ -z $unix ] && unix=/var/www/mystocks-go/mystocks-go.sock
+    [ -z $unix ] && unix=/run/mystocks-go.sock
     read -p 'Please enter host(default: 127.0.0.1): ' host
     [ -z $host ] && host=127.0.0.1
     read -p 'Please enter port(default: 12345): ' port
