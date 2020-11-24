6ce2b548c91f00f44a655ff4aceb2fe9492e7f1c
sunshineplan <sunshineplan@users.noreply.github.com>
2020-11-24 17:08:08 +0800

    Fix Maximum call stack size exceeded

--- webapp/static/components/stock.js
+++ webapp/static/components/stock.js
@@ -4,18 +4,28 @@ const stock = {
       refresh: Number(app.dataset.refresh) + 1,
       autoUpdate: [],
       stock: {},
-      chart: ''
+      chart: '',
+      data: [],
+      update: ''
     }
   },
   computed: {
     index() { return this.$route.params.index },
     code() { return this.$route.params.code }
   },
+  watch: {
+    update() {
+      document.title = `${this.stock.name} ${this.stock.now} ${this.stock.percent}`
+      updateChart()
+    }
+  },
   provide() { return { Stock: Vue.computed(() => this.stock) } },
   watch: {
-    $route(to) {
-      if (to.name == 'stock' && this.code != 'n/a')
-        this.loadChart(true).then(() => this.loadRealtime(true))
+    async $route(to) {
+      if (to.name == 'stock' && this.code != 'n/a') {
+        await this.loadChart(true)
+        await this.loadRealtime(true)
+      }
     }
   },
   components: { autocomplete, realtime, chart },
@@ -36,53 +46,48 @@ const stock = {
   mounted() {
     document.title = 'My Stocks'
     this.start()
-    this.chart = new Chart(stockChart, intraday)
   },
   beforeUnmount() {
     for (; this.autoUpdate.length > 0;) clearInterval(this.autoUpdate.pop())
     this.chart.destroy()
   },
   methods: {
-    start() {
+    async start() {
+      this.chart = new Chart(stockChart, intraday)
       if (this.code != 'n/a') {
-        this.loadChart(true).then(() => this.loadRealtime(true))
-        this.autoUpdate.push(setInterval(() => this.loadRealtime(), this.refresh * 1000))
-        this.autoUpdate.push(setInterval(() => this.loadChart(), 60000))
+        await this.loadChart(true)
+        await this.loadRealtime(true)
+        document.title = `${this.stock.name} ${this.stock.now} ${this.stock.percent}`
+        this.chart.options.scales.yAxes[0].ticks.suggestedMin = this.stock.last / 1.01
+        this.chart.options.scales.yAxes[0].ticks.suggestedMax = this.stock.last * 1.01
+        this.chart.annotation.elements.PreviousClose.options.value = this.stock.last
+        this.updateChart()
+        //this.autoUpdate.push(setInterval(this.loadRealtime, this.refresh * 1000))
+        //this.autoUpdate.push(setInterval(this.loadChart, 60000))
+        this.autoUpdate.push(setInterval(this.loadRealtime, 500, true))
+        this.autoUpdate.push(setInterval(this.loadChart, 500, true))
       }
     },
-    loadRealtime(force) {
-      if (checkTime() || force && this.code)
-        return post('/get', { index: this.index, code: this.code, q: 'realtime' })
-          .then(response => response.json())
-          .then(stock => {
-            this.stock = stock
-            if (stock && stock.name != 'n/a') {
-              document.title = `${stock.name} ${stock.now} ${stock.percent}`
-              var last = this.chart.data.datasets[0].data
-              if (last.length) {
-                last[last.length - 1].y = stock.now
-                this.chart.update()
-              }
-            }
-          })
-    },
-    loadChart(force) {
+    async loadRealtime(force) {
       if (checkTime() || force && this.code) {
-        return post('/get', { index: this.index, code: this.code, q: 'chart' })
-          .then(response => response.json()).then(json => {
-            if (json) {
-              if (json.chart)
-                this.chart.data.datasets.forEach(dataset => {
-                  dataset.data = json.chart
-                })
-              if (json.last != 0) {
-                this.chart.options.scales.yAxes[0].ticks.suggestedMin = json.last / 1.01
-                this.chart.options.scales.yAxes[0].ticks.suggestedMax = json.last * 1.01
-                this.chart.annotation.elements.PreviousClose.options.value = json.last
-              }
-              this.chart.update()
-            }
-          })
+        let response = await post('/get', { index: this.index, code: this.code, q: 'realtime' })
+        this.stock = await response.json()
+        this.update = this.stock.update
+      }
+    },
+    async loadChart(force) {
+      if (checkTime() || force && this.code) {
+        let response = await post('/get', { index: this.index, code: this.code, q: 'chart' })
+        let json = await response.json()
+        this.data = json.chart
+      }
+    },
+    updateChart() {
+      if (this.data.length) {
+        let data = this.data
+        data[data.length - 1].y = this.stock.now
+        this.chart.data.datasets[0].data = data
+        this.chart.update()
       }
     }
   }
