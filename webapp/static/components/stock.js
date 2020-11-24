a9eb9309bd1041ba871ee3e9b98414460ea9af49
sunshineplan <sunshineplan@users.noreply.github.com>
2020-11-24 18:45:54 +0800

    Update stock.js

--- webapp/static/components/stock.js
+++ webapp/static/components/stock.js
@@ -1,4 +1,5 @@
 const stock = {
+  provide() { return { Stock: Vue.computed(() => this.stock) } },
   data() {
     return {
       refresh: Number(app.dataset.refresh) + 1,
@@ -14,19 +15,8 @@ const stock = {
     code() { return this.$route.params.code }
   },
   watch: {
-    update() {
-      document.title = `${this.stock.name} ${this.stock.now} ${this.stock.percent}`
-      updateChart()
-    }
-  },
-  provide() { return { Stock: Vue.computed(() => this.stock) } },
-  watch: {
-    async $route(to) {
-      if (to.name == 'stock' && this.code != 'n/a') {
-        await this.loadChart(true)
-        await this.loadRealtime(true)
-      }
-    }
+    update(_, last) { if (last != '') this.updateChart() },
+    async $route(to) { if (to.name == 'stock' && this.code != 'n/a') await this.load() }
   },
   components: { autocomplete, realtime, chart },
   template: `
@@ -55,35 +45,39 @@ const stock = {
     async start() {
       this.chart = new Chart(stockChart, intraday)
       if (this.code != 'n/a') {
-        await this.loadChart(true)
-        await this.loadRealtime(true)
-        document.title = `${this.stock.name} ${this.stock.now} ${this.stock.percent}`
-        this.chart.options.scales.yAxes[0].ticks.suggestedMin = this.stock.last / 1.01
-        this.chart.options.scales.yAxes[0].ticks.suggestedMax = this.stock.last * 1.01
-        this.chart.annotation.elements.PreviousClose.options.value = this.stock.last
-        this.updateChart()
-        //this.autoUpdate.push(setInterval(this.loadRealtime, this.refresh * 1000))
-        //this.autoUpdate.push(setInterval(this.loadChart, 60000))
-        this.autoUpdate.push(setInterval(this.loadRealtime, 500, true))
-        this.autoUpdate.push(setInterval(this.loadChart, 500, true))
+        await this.load()
+        this.autoUpdate.push(setInterval(this.loadRealtime, this.refresh * 1000))
+        this.autoUpdate.push(setInterval(this.loadChart, 60000))
       }
+    },
+    async load() {
+      await this.loadRealtime(true)
+      await this.loadChart(true)
+      this.chart.options.scales.yAxes[0].ticks.suggestedMin = this.stock.last / 1.01
+      this.chart.options.scales.yAxes[0].ticks.suggestedMax = this.stock.last * 1.01
+      this.chart.annotation.elements.PreviousClose.options.value = this.stock.last
+      this.updateChart()
     },
     async loadRealtime(force) {
       if (checkTime() || force && this.code) {
         let response = await post('/get', { index: this.index, code: this.code, q: 'realtime' })
-        this.stock = await response.json()
-        this.update = this.stock.update
+        let stock = await response.json()
+        if (stock.name) {
+          this.stock = stock
+          this.update = stock.update
+          document.title = `${stock.name} ${stock.now} ${stock.percent}`
+        }
       }
     },
     async loadChart(force) {
       if (checkTime() || force && this.code) {
         let response = await post('/get', { index: this.index, code: this.code, q: 'chart' })
         let json = await response.json()
-        this.data = json.chart
+        if (json.chart) this.data = json.chart
       }
     },
     updateChart() {
-      if (this.data.length) {
+      if (this.data.length && this.stock.now) {
         let data = this.data
         data[data.length - 1].y = this.stock.now
         this.chart.data.datasets[0].data = data
