2aebc01d1780762f0f43678fa4429fb7b0d4370c
sunshineplan <sunshineplan@users.noreply.github.com>
2025-10-27 11:18:59 +0800

    Remove retry logic from test files (#126)
    Eliminated usage of the retry package in capitalflows_test.go and eastmoney_test.go. Tests now directly call the functions under test and check results, simplifying the test code and dependencies.

--- capitalflows/capitalflows_test.go
+++ capitalflows/capitalflows_test.go
@@ -3,16 +3,11 @@ package capitalflows
 import (
 	"log"
 	"testing"
-
-	"github.com/sunshineplan/utils/retry"
 )
 
 func TestFlows(t *testing.T) {
-	var flows CapitalFlows
-	if err := retry.Do(func() (err error) {
-		flows, err = Fetch()
-		return
-	}, 5, 20); err != nil {
+	flows, err := Fetch()
+	if err != nil {
 		t.Fatal(err)
 	}
 	log.Println(len(flows), flows)
